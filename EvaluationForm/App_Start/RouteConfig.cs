de24a8c3f19a1fc040238f26a80b363110e604f9
ashcse <ashcse.khare@gmail.com>
2017-05-09 13:42:25 +0530

    Added solution for EvalForm

--- EvaluationForm/App_Start/RouteConfig.cs
+++ EvaluationForm/App_Start/RouteConfig.cs
@@ -13,10 +13,15 @@ namespace EvaluationForm
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+
+                 routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Employees", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ EvaluationForm/Models/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EvaluationForm.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 4)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 4)]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 10)]
+        public string Department { get; set; }
+
+        //[Required]
+        //[StringLength(20, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 3)]
+        //public string Category { get; set; }
+
+        //[Required]
+        //public decimal? Price { get; set; }
+    }
+}
